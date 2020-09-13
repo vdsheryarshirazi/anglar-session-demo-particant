959b1f54fb5a94d9a4ef00d511f0e79b8fd50a21
Sheryar Shirazi <shery@Sheryar-Shirazi-MB-Pro.local>
2020-09-13 14:30:43 +0500

    add fundamental assignment finish

--- src/app/fish/fish.component.ts
+++ src/app/fish/fish.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChange, SimpleChanges, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-fish',
   templateUrl: './fish.component.html',
   styleUrls: ['./fish.component.scss'],
 })
-export class FishComponent {
+export class FishComponent implements OnChanges, OnDestroy {
   @Input() speed: number;
   @Input() depth: number;
   @Input() src: string;
@@ -20,5 +20,55 @@ export class FishComponent {
     this.left = 0;
     this.depth = 0;
     this.isL2R = true;
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    // speed changes
+    if (changes.speed) {
+      const { currentValue, previousValue } = changes.speed;
+      if (currentValue !== previousValue) {
+        if (this.speedTimer) {
+          this.clearTimer();
+        }
+        this.setTimer();
+      }
+    }
+    // swimming changes
+    if (changes.isSwimming) {
+      if (changes.isSwimming.currentValue) {
+        if (!this.speedTimer) {
+          this.setTimer();
+        }
+      } else if (!changes.isSwimming.currentValue) {
+        this.clearTimer();
+      }
+    }
+  }
+  ngOnDestroy(): void {
+    this.clearTimer();
+  }
+  swim(): void {
+    if (this.left === 90) {
+      this.isL2R = false;
+    }
+    if (this.left === 10) {
+      this.isL2R = true;
+    }
+
+    if (this.isL2R) {
+      this.left += 1;
+    } else {
+      this.left -= 1;
+    }
+  }
+  setTimer(): void {
+    this.speedTimer = setInterval(() => {
+      console.log(`${this.name} swimming ${this.depth}`);
+      this.swim();
+    }, this.speed);
+  }
+  clearTimer(): void {
+    clearInterval(this.speedTimer);
+    this.speedTimer = null;
   }
 }
--- src/app/pound/pound.component.ts
+++ src/app/pound/pound.component.ts
@@ -6,6 +6,7 @@ interface IFish {
   src: string;
   depth: number;
   speed: number;
+  caught: boolean;
 }
 
 @Component({
@@ -24,6 +25,7 @@ export class PoundComponent {
         src: 'fish1.gif',
         depth: 0,
         speed: 100,
+        caught: false,
       },
       {
         id: 2,
@@ -32,6 +34,7 @@ export class PoundComponent {
         src: 'fish2.gif',
         depth: 7,
         speed: 300,
+        caught: false,
       },
       {
         id: 3,
@@ -40,12 +43,26 @@ export class PoundComponent {
         src: 'turtle.gif',
         depth: 16,
         speed: 500,
+        caught: false,
       },
     ];
   }
-  catchFish(i: number): void {}
-  stop(index: number): void {}
-  start(index: number): void {}
-  incSpeed(index: number): void {}
-  decSpeed(index: number): void {}
+  catchFish(i: number): void {
+    // this.fishes.splice(i, 1);
+    this.fishes[i].caught = true;
+  }
+  stop(index: number): void {
+    this.fishes[index].swimming = false;
+  }
+  start(index: number): void {
+    this.fishes[index].swimming = true;
+  }
+  incSpeed(index: number): void {
+    if (this.fishes[index].speed - 50 > 0) {
+      this.fishes[index].speed -= 50;
+    }
+  }
+  decSpeed(index: number): void {
+    this.fishes[index].speed += 50;
+  }
 }
