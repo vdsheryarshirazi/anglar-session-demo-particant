010d1bc77268b5f72a49596979b9c0f0bb4cbf5b
Sheryar Shirazi <shery@Sheryar-Shirazi-MB-Pro.local>
2020-10-04 19:37:38 +0500

    fix typing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,21 +7,21 @@ import { shades, IShade } from './data/shades';
 })
 export class AppComponent {
   shades: IShade[];
-  encoding: boolean;
-  options: { display: string; value: boolean }[] = [
+  encoding: string;
+  options: { display: string; value: string }[] = [
     {
-      value: true,
+      value: 'true',
       display: 'Encoding',
     },
     {
-      value: false,
+      value: 'false',
       display: 'Decoding',
     },
   ];
   inputText: string;
   constructor() {
     this.shades = shades;
-    this.encoding = true;
+    this.encoding = 'true';
     this.inputText = '';
   }
 }
