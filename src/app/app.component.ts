ebd93d30b9248cf134558fd7b2ea4d76ccedaac8
Sheryar Shirazi <shery@Sheryar-Shirazi-MB-Pro.local>
2020-10-04 18:50:24 +0500

    assignment start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
-
+import { shades, IShade } from './data/shades';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-session-demo';
+  shades: IShade[];
+  encoding: boolean;
+  options: { display: string; value: boolean }[] = [
+    {
+      value: true,
+      display: 'Encoding',
+    },
+    {
+      value: false,
+      display: 'Decoding',
+    },
+  ];
+  inputText: string;
+  constructor() {
+    this.shades = shades;
+    this.encoding = true;
+    this.inputText = '';
+  }
 }
---
+++ src/app/data/shades.ts
@@ -0,0 +1,32 @@
+export interface IShade {
+  color: string;
+}
+export const shades = [
+  {
+    color: 'red',
+  },
+  {
+    color: 'yellow',
+  },
+  {
+    color: 'grey',
+  },
+  {
+    color: 'green',
+  },
+  {
+    color: 'teal',
+  },
+  {
+    color: 'gold',
+  },
+  {
+    color: 'pink',
+  },
+  {
+    color: 'orange',
+  },
+  {
+    color: 'aqua',
+  },
+];
